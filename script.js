14560afffe1cd1d435ee239b7b0828abc7073364
Mustafa <koc.mustafaa1@gmail.com>
2025-11-21 08:43:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function convertCtoF(celsius) {
+ return celsius * 9/5 + 32;
+}
+
+console.log(convertCtoF(0)); 
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));
